691a32626d9f7e097e4dafe5379bba64f93c97cb
Ofek-GitHub <ofeksimantov53@gmail.com>
2022-12-20 13:16:01 +0200

    added few more

---
+++ addTwoNumbers.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)
+
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
+
+
+solution = Solution()
+print(solution.addTwoNumbers([2, 4, 3], [5, 6, 4]))
